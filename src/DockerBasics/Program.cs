cf0ac615979cb16b91c38f1a7cb8dadd178e9f6a
Максим <shadrin.maksim.sm@gmail.com>
2025-09-21 02:45:13 +0300

    feat: реализован сервис и добавлен эндпоинт health для урока «Основы работы с Docker»

---
+++ src/DockerBasics/Program.cs
@@ -0,0 +1,18 @@
+
+namespace DockerBasics;
+
+public abstract class Program()
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        
+        var app = builder.Build();
+
+        app.MapControllers();
+        
+        app.Run();
+    }
+}
