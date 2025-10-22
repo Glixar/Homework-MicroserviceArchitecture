4bf08c8f50ccc04ec544dd870044354b218cbe27
Максим <shadrin.maksim.sm@gmail.com>
2025-10-22 14:47:41 +0300

    feat(HealthService): добавить микросервис HealthService

---
+++ src/HealthService/Program.cs
@@ -0,0 +1,17 @@
+namespace HealthService;
+
+public abstract class Program()
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        
+        var app = builder.Build();
+
+        app.MapControllers();
+        
+        app.Run();
+    }
+}
